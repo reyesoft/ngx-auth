ec5631650ca8a5f2a5bfe3bfe18d83df792a785b
max <max.ss@outlook.com>
2019-07-29 09:02:00 -0300

    implementado el nuevo custom validator

--- projects/auth/src/lib/guest-start/sign-up/sign-up.component.ts
+++ projects/auth/src/lib/guest-start/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { LowerCasePipe } from '@angular/common';
 import { MediaObserver } from '@angular/flex-layout';
@@ -11,6 +11,7 @@ import { GuestStartService } from '../services/guest-start.service';
 // ngx-formly
 import { signup_form } from './signup-form.model';
 import { FormlyFieldConfig } from '@ngx-formly/core';
+import { CustomValidators } from 'ngx-jsonapi-material';
 
 @Component({
     selector: 'auth-sign-up',
@@ -21,17 +22,46 @@ export class SignUpComponent {
     public form = new FormGroup({});
     public model: { [key: string]: any } = {};
     public fields: Array<FormlyFieldConfig> = signup_form;
+    private custom_validators = new CustomValidators();
 
     protected user_login: { email?: string; password?: string } = {};
 
     public constructor(
         public mediaObserver: MediaObserver,
+        protected formBuilder: FormBuilder,
         protected guestStartService: GuestStartService,
         protected lowercase: LowerCasePipe,
         protected router: Router
-    ) {}
+    ) {
+        this.form = this.createSignupForm();
+    }
 
     public registerUser(): void {
         this.guestStartService.register(this.form);
     }
+
+    private createSignupForm(): FormGroup {
+      return this.formBuilder.group(
+          {
+              email: [
+                  null,
+                  Validators.compose([
+                      Validators.email,
+                      Validators.required
+                  ])
+              ],
+              password: [
+                  null,
+                  Validators.compose([
+                      Validators.required,
+                      Validators.minLength(8)
+                  ])
+              ],
+              confirm_password: [null, Validators.compose([Validators.required])]
+          },
+          {
+              // check whether our password and confirm password match
+              validator: this.custom_validators.passwordMatchValidator
+          });
+  }
 }
