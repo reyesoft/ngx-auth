81a850e80b37d678046d1bdd36da8588aef24dfc
Matias Ahumada <ahumadamatias7@gmail.com>
2020-11-23 11:34:55 -0300

    autocommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,10 +34,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
                 reset_password: { route: 'resetpassword' }
             },
             class_actions_button: 'button-actions',
-            social_buttons: [
-                { key: 'facebook', color: 'blue', svgIcon: 'facebook', text: 'Iniciar con Facebook', classes: 'button-secondary' },
-                { key: 'google', color: 'white', svgIcon: 'google', text: 'Iniciar con Google', classes: 'button-secondary' }
-            ],
             // afterOAuthLoginMethod: (data: {[key: string]: any}) => { console.log('login data ----------->', data); },
             // afterOAuthRefreshMethod: (data: {[key: string]: any}) => { console.log('refresh data ----------->', data); },
             // registerUser: (data) => { console.log('data ----------->', data); },
